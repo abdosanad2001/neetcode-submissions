899e7c95e67a6f686abf57547a27a1c430f40094
abdosanad2001 <abdelalisanad@gmail.com>
2026-07-29 16:52:41 +0100

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        //dp[i]=no of ways to climb staris
+       if(n<=2){return n;}
+        vector<long long> dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+
+            dp[i]+=(dp[i-1]+dp[i-2]);
+        }
+        return dp[n];
+    }
+};
