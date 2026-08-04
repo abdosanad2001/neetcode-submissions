f5aec6118f74f81f56183a7d5281724632e1becb
abdosanad2001 <abdelalisanad@gmail.com>
2026-08-04 23:03:13 +0100

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int l=0,i=0;
+        int r=n-1;
+        while(i<=r){
+            if (nums[i]==0){
+                swap(nums[i],nums[l]);
+                l++;
+            }
+            if(nums[i]==2){
+                swap(nums[i],nums[r]);
+                r--;
+                i--;
+            }
+            i++;
+        }
+        
+    }
+};
