9d190c00edf706305699218e67b8e94a090d350f
abdosanad2001 <abdelalisanad@gmail.com>
2026-08-11 09:26:39 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string new_s="";
+        for(int i=0;i<s.size();i++){
+            s[i]=tolower(s[i]);
+            if(!isalnum(s[i])) continue;
+            else new_s+=s[i];
+        }
+
+        int n=new_s.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            if(new_s[l]!=new_s[r]){
+                return false;
+            }
+            swap(new_s[l],new_s[r]);
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
